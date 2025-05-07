4b504bf0485ab75565468640d86377caf693e51d
Gabriel <gabibeinotti13@gmail.comm>
2025-05-07 00:34:15 -0300

    modificación

--- App.py
+++ App.py
@@ -2,7 +2,15 @@ import streamlit as st
 from cargar_audio import mostrar_carga
 from grabar_audio import mostrar_grabacion
 
+import subprocess
 
+try:
+    subprocess.run(["ffmpeg", "-version"], check=True)
+    print("ffmpeg está instalado correctamente")
+except FileNotFoundError:
+    print("ffmpeg no está instalado")
+    st.error("❌ ffmpeg no está instalado. Por favor, instálalo para usar esta aplicación.")
+    
 st.title("Detector de fluidez verbal")
 st.write("Esta aplicación permite cargar o grabar un audio y analizar su fluidez verbal.")
 
